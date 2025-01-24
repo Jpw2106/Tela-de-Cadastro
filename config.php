c7b6e7ce936b30e957b159e7be84ce333121b7f0
unknown <you@example.com>
2025-01-24 17:44:37 -0300

    Adição de PHP no projeto

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+   $dbHost = 'localhost';
+   $dbuserName = 'root';
+   $dbPassword = '';
+   $dbName = 'formulario_joao'; 
+
+   $conexao = new mysqli($dbHost, $dbuserName, $dbPassword, $dbName);
+
+
+  // if($conexao->connect_error) 
+   //     echo "Erro: " . $conexao->connect_error; 
+   //else
+  // {
+  //      echo "Conexão Efetuada com Sucesso"; 
+  //}
+
+    ?>
